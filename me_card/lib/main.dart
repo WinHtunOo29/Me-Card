b60ce004a934c0c7b5c974c7156b06b6224f4949
Win Htun Oo <winhtunoo@onenex.co>
2024-06-25 16:51:29 +0700

    Me card done

--- me_card/lib/main.dart
+++ me_card/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,23 +8,71 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp (
+    return MaterialApp (
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              CircleAvatar(
+              const CircleAvatar(
                 radius: 50,
                 backgroundImage: AssetImage('images/me.jpg'),
               ),
-              Text(
+              const Text(
                 'Win Htun Oo',
                 style: TextStyle(
+                  fontFamily: 'Pacifico',
                   color: Colors.white,
                   fontSize: 40,
                   fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Text(
+                'iOS Developer/ Flutter Developer',
+                style: TextStyle(
+                  fontFamily: 'Source Sans 3',
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                child: ListTile(
+                  leading: const Icon(Icons.phone, color: Colors.teal),
+                  title: Text(
+                    '+66 616 728 409',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans 3',
+                      fontWeight: FontWeight.w900,
+                      fontSize: 18
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                child: ListTile(
+                  leading: const Icon(Icons.email, color: Colors.teal),
+                  title: Text(
+                    'winhtunoo.who@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans 3',
+                      fontWeight: FontWeight.w900,
+                      fontSize: 18,
+                    ),  
+                  ),
                 ),
               ),
             ],
@@ -35,4 +82,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
